556be108e4a99f86e3661e488fe6be44d85422a8
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-10-27 12:58:44 +0100

    feat: add task extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,17 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
+function repeater(str, options) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = options.hasOwnProperty('separator') ? String(options.separator) : '+';
+  let addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let additionSeparator = options.hasOwnProperty('additionSeparator') && addition ? String(options.additionSeparator) : '|';
+
+  const newStr = String(str) + Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+  return Array(repeatTimes).fill(newStr).join(separator);
 }
 
 module.exports = {
