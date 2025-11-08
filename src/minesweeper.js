7891b4a7f4aae3fd1c8f6566f6320489fac580e4
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-11-08 20:41:36 +0100

    feat: task minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -23,9 +23,34 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
+function minesweeper(matrix) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  const n = matrix.length;
+  const m = matrix[0].length;
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    result[i] = [];
+    for (let j = 0; j < m; j++) {
+      result[i][j] = 0;
+    }
+  }
+
+  for (i = 0; i < n; i += 1) {
+    for (j = 0; j < m; j += 1) {
+      let cell = 0;
+ try { cell += (matrix[i + 1][j] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i + 1][j + 1] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i][j + 1] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i - 1][j + 1] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i - 1][j] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i - 1][j - 1] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i][j - 1] === true) ? 1 : 0; } catch {}
+      try { cell += (matrix[i + 1][j - 1] === true) ? 1 : 0; } catch {}
+      result[i][j] = cell;
+    }
+  }
+  return result;
 }
 
 module.exports = {
